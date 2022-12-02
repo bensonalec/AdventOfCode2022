e2cb2a0a6cafe034a7f55065a46acf376186598d
Alec Benson <alecbenson21@gmail.com>
2022-12-01 23:15:43 -0700

    Finished day 2

--- day2/day2.py
+++ day2/day2.py
@@ -1,26 +1,45 @@
-from aocd import lines, submit
+rules = {
+    "Rock" : ["Paper", "Rock", "Scissors"],
+    "Paper": ["Scissors", "Paper", "Rock"],
+    "Scissors": ["Rock", "Scissors", "Paper"]
+}
+weights = {
+    "Rock": 1,
+    "Paper": 2,
+    "Scissors": 3
+}
 
-def preProcess(inp):
-    inp.split("\n")
-    data = [line.strip() for line in inp]
-    data = [x for x in data if x != ""]
-    return data
+scoreTable = [0, 3, 6]
+scoreTableTwo = ["X", "Y", "Z"]
 
-def partOne(inp):
-    pass
+def partOne():
+    with open("sample.txt") as fi:
+        readIn = fi.read()
+    readIn = readIn.replace("X", "Rock")
+    readIn = readIn.replace("Y", "Paper")
+    readIn = readIn.replace("Z", "Scissors")
+    readIn = readIn.replace("A", "Rock")
+    readIn = readIn.replace("B", "Paper")
+    readIn = readIn.replace("C", "Scissors")
+    score = 0
+    for i in readIn.split("\n"):
+        ans = i.split()
+        score += weights[ans[1]] + scoreTable[rules[ans[1]].index(ans[0])]
+    print("Part One: ", score)
 
-def partTwo(inp):
-    pass
+def partTwo():
+    with open("sample.txt") as fi:
+        readIn = fi.read()
+    readIn = readIn.replace("A", "Rock")
+    readIn = readIn.replace("B", "Paper")
+    readIn = readIn.replace("C", "Scissors")
+    finalScore = 0
+    for i in readIn.split("\n"):
+        ans = i.split()
+        toChoose = rules[ans[0]][scoreTableTwo[::-1].index(ans[1])]
+        finalScore += weights[toChoose] + scoreTable[scoreTableTwo.index(ans[1])]
+    print("Part Two: ", finalScore)
 
 #use sample input
-with open("sample.txt") as fi:
-    readIn = fi.read()
-partOne(preProcess(readIn))
-partTwo(preProcess(readIn))
-
-
-#submit your input
-from aocd import get_data
-data = get_data(day=2, year=2022)
-submit(partOne(preProcess(data)))
-submit(partTwo(preProcess(data)))
+partOne()
+partTwo()
