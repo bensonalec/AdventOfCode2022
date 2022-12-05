2ad6b1b9c6cd6de4cfe996e2721f89c6b2404eda
Alec Benson <alecbenson21@gmail.com>
2022-12-04 17:24:43 -0700

    Added elixir solution for day 3

---
+++ day3/day3.ex
@@ -0,0 +1,28 @@
+#Part One
+getPriority = fn
+  <<x>> when x > 95 -> x - 96
+  <<x>> -> x - 64 + 26
+end
+
+getInCommonItems = fn
+  x ->
+    x = String.graphemes(x)
+    val = MapSet.to_list(MapSet.intersection(MapSet.new(Enum.slice(x, 0, div(length(x), 2))),  MapSet.new(Enum.slice(x, div(length(x), 2), 100))))
+    Enum.sum(Enum.map(val, getPriority))
+end
+
+IO.puts Enum.sum(Enum.map(String.split(File.read!("sample.txt"), "\n"), getInCommonItems))
+
+
+#Part Two
+getInCommonItems = fn
+  x ->
+    pOne = String.graphemes(Enum.at(x, 0))
+    pTwo = String.graphemes(Enum.at(x, 1))
+    pThree = String.graphemes(Enum.at(x, 2))
+    val = MapSet.to_list(MapSet.intersection(MapSet.intersection(MapSet.new(pOne),  MapSet.new(pTwo)), MapSet.new(pThree)))
+    Enum.sum(Enum.map(val, getPriority))
+end
+
+
+IO.puts Enum.sum(Enum.map(Enum.chunk_every(String.split(File.read!("sample.txt"), "\n"), 3), getInCommonItems))
