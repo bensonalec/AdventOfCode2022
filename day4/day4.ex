b50a2d78797499f20f4bd36e19735c82703ed724
Alec Benson <alecbenson21@gmail.com>
2022-12-04 18:12:02 -0700

    Implemented elixir solution for day 4

---
+++ day4/day4.ex
@@ -0,0 +1,20 @@
+#Part One
+determineTotalOverlap = fn
+  [x, y, i, j] when i <= x and x <= j and i <= y and y <= j  -> 1
+  [x, y, i, j] when x <= i and i <= y and x <= j and j <= y -> 1
+  [_, _, _, _] -> 0
+end
+
+#Part Two
+determineAnyOverlap = fn
+  [x, y, i, j] when (i <= x and x <= j) or (i <= y and y <= j)  -> 1
+  [x, y, i, j] when (x <= i and i <= y) or (x <= j and j <= y)  -> 1
+  [_, _, _, _] -> 0
+end
+
+parseLine = fn
+  x -> Enum.map(Enum.slice(Regex.run(~r/(\d+)-(\d+),(\d+)-(\d+)/, x), 1, 100), &Integer.parse/1)
+end
+
+IO.puts Enum.sum(Enum.map(Enum.map(String.split(File.read!("sample.txt"), "\n"), parseLine), determineTotalOverlap))
+IO.puts Enum.sum(Enum.map(Enum.map(String.split(File.read!("sample.txt"), "\n"), parseLine), determineAnyOverlap))
