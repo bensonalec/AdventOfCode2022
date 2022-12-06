3a9926d88717765f3d76c8ac6a07f385be77e648
Alec Benson <alecbenson21@gmail.com>
2022-12-05 22:37:19 -0700

    Cleaned code up a tiny bit

--- day6/day6.ex
+++ day6/day6.ex
@@ -4,22 +4,22 @@ defmodule DaySix do
 
   determineSignal = fn
     content, maxNum ->
-      Enum.filter(Enum.map(Enum.with_index(content), fn {x, ind} ->
-        current = x
-        toLoop = 0..(maxNum - 1)
-        toCheck = Enum.map(toLoop, fn x -> Enum.at(content, ind + x) end)
+      Enum.filter(
+        Enum.map(Enum.with_index(content), fn {_, ind} ->
+          toCheck = Enum.map(0..(maxNum - 1), fn x -> Enum.at(content, ind + x) end)
 
-        if length(Enum.uniq(toCheck)) == maxNum do
-          ind + 1 + maxNum - 1
-        else
-          nil
-        end
-      end), &(!is_nil(&1)))
+          if length(Enum.uniq(toCheck)) == maxNum do
+            ind + 1 + maxNum - 1
+          else
+            nil
+          end
+        end),
+        &(!is_nil(&1))
+      )
   end
 
   [head | _] = determineSignal.(content, 4)
   IO.inspect(head)
   [head | _] = determineSignal.(content, 14)
   IO.inspect(head)
-
 end
