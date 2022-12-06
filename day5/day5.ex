3cbffb151c39fe056ca47b4ae6da6d3d3e5beef9
Alec Benson <alecbenson21@gmail.com>
2022-12-05 21:51:09 -0700

    Adding day 5 answers

--- day5/day5.ex
+++ day5/day5.ex
@@ -7,6 +7,7 @@ defmodule DayFive do
       {num, ""} = Integer.parse(num)
       from = from - 1
       to = to - 1
+
       Enum.map(Enum.with_index(stack), fn
         {x, ind} when ind == from -> Enum.slice(x, num, 100)
         {x, ind} when ind == to -> Enum.reverse(Enum.slice(Enum.at(stack, from), 0, num)) ++ x
@@ -22,6 +23,7 @@ defmodule DayFive do
       {num, ""} = Integer.parse(num)
       from = from - 1
       to = to - 1
+
       Enum.map(Enum.with_index(stack), fn
         {x, ind} when ind == from -> Enum.slice(x, num, 100)
         {x, ind} when ind == to -> Enum.slice(Enum.at(stack, from), 0, num) ++ x
@@ -35,11 +37,27 @@ defmodule DayFive do
   {maxBin, ""} = Integer.parse(Enum.at(maxbin, 1))
   instructions = Regex.scan(~r/move (\d+) from (\d+) to (\d+)/, content)
   bins = Enum.chunk_every(bins, maxBin)
+
   initialStack =
     Enum.map(0..(maxBin - 1), fn n ->
       Enum.map(0..(length(bins) - 1), fn i -> Enum.at(Enum.at(Enum.at(bins, i), n), 1) end)
     end)
+
   initialStack = Enum.map(initialStack, fn x -> Enum.filter(x, &(!is_nil(&1))) end)
-  IO.puts List.to_string(Enum.map(Enum.reduce(instructions, initialStack, runInstructionPartOne), fn x -> Enum.at(x, 0) end))
-  IO.puts List.to_string(Enum.map(Enum.reduce(instructions, initialStack, runInstructionPartTwo), fn x -> Enum.at(x, 0) end))
+
+  IO.puts(
+    List.to_string(
+      Enum.map(Enum.reduce(instructions, initialStack, runInstructionPartOne), fn x ->
+        Enum.at(x, 0)
+      end)
+    )
+  )
+
+  IO.puts(
+    List.to_string(
+      Enum.map(Enum.reduce(instructions, initialStack, runInstructionPartTwo), fn x ->
+        Enum.at(x, 0)
+      end)
+    )
+  )
 end
