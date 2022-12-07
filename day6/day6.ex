15c708c3e68b3ff54f903c221b19f329499575d7
Alec Benson <alecbenson21@gmail.com>
2022-12-06 21:38:30 -0700

    Formatted answer from yesterday

--- day6/day6.ex
+++ day6/day6.ex
@@ -1,8 +1,12 @@
 defmodule DaySix do
   findIndex = fn
-    maxNum, content ->  Enum.find_index(Enum.chunk_every(content, maxNum, 1, :discard), fn x -> length(Enum.uniq(x)) == maxNum end) + maxNum
+    maxNum, content ->
+      Enum.find_index(Enum.chunk_every(content, maxNum, 1, :discard), fn x ->
+        length(Enum.uniq(x)) == maxNum
+      end) + maxNum
   end
-  content = "sample.txt" |> File.read! |> String.graphemes
-  IO.inspect findIndex.(4, content)
-  IO.inspect findIndex.(14, content)
+
+  content = "sample.txt" |> File.read!() |> String.graphemes()
+  IO.inspect(findIndex.(4, content))
+  IO.inspect(findIndex.(14, content))
 end
